ff2b481ad38526175104c6e77faed8abaaa1c8be
Thomas Suarez <code@tomthecarrot.com>
2021-10-09 22:25:12 -0400

    [SER-231] Add ResultCode enum and update ffi function signatures

--- platform/src/ffi.rs
+++ platform/src/ffi.rs
@@ -1,24 +1,33 @@
 use core::ffi::c_void;
 
+type ObjectHandle = u16;
 type StateHandle = u16;
 type DataType = u8;
 
 static mut TEST_VALUE: i8 = -50;
-static mut TEST_STRING: String = String::new();
-static mut TEST_STRING_IS_READY: bool = false; 
 
 #[no_mangle]
-pub extern "C" fn tp_state_get_ptr(state_handle: StateHandle, data_type: u8, result: *mut u8) -> ResultCode {
-    let mut result_code: ResultCode = RESULT_FAIL_UNKNOWN;
+pub extern "C" fn tp_property_get_ptr(object_handle: ObjectHandle, property_handle: StateHandle, data_type: u8, result_ptr: *mut u8) -> TPResultCode {
+    let mut result_code: TPResultCode = TPResultCode::RESULT_FAIL_UNKNOWN;
 
     unsafe {
-        let data_value = &STORE.test_set.int8; // TEMP
-        let data_ptr = data_value as *const i8;
-        let generic_ptr = data_ptr as *const u8;
+        let data_value = TEST_VALUE; // TEMP
+        let data_ptr = data_value as *const i8; // TEMP
+        let generic_ptr = data_ptr as *const u8; // TEMP
 
-        *result = *generic_ptr; // MARK[UNSAFE_NEEDED]
+        *result_ptr = *generic_ptr; // MARK[UNSAFE_NEEDED]
         result_code = RESULT_OK; // MARK[UNSAFE_NEEDED]
     }
     
     result_code
+}
+
+#[no_mangle]
+pub extern "C" fn tp_property_flag() {
+    // TODO
+}
+
+#[no_mangle]
+pub extern "C" fn tp_property_arm(object_handle: ObjectHandle, property_handle: StateHandle, armed: bool) -> TPResultCode {
+    // TODO
 }
---
+++ platform/src/result_code.rs
@@ -0,0 +1,7 @@
+pub enum TPResultCode
+{
+    OK,
+    FAIL_UNKNOWN,
+    INVALID_DATA_TYPE,
+    INVALID_POINTER,
+}
