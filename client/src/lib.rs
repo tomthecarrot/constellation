dae0de95043d5d57d5cba2047c4802c9fee0167e
Ryan Butler <thebutlah@gmail.com>
2021-11-05 16:42:43 -0400

    Made arena crate

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,5 +1,4 @@
 mod contract;
-mod datastructures;
 mod flatb;
 mod link;
 mod object;
--- client/src/properties/state.rs
+++ client/src/properties/state.rs
@@ -1,5 +1,9 @@
-use crate::datastructures::arena;
-
 pub struct State;
 
 pub type StateID = arena::Index<State>;
+
+/// Represents a particular state of a contract. For actual state data of a
+/// specific object, see [`StateID`].
+pub struct StateProperty {
+    idx: usize,
+}
