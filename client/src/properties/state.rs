ad0244d3f4428d82f51872ed6e4e87e9ad6d18d6
Ryan Butler <thebutlah@gmail.com>
2021-11-08 16:31:00 -0500

    Fixed arena::Index trait derives, worked on StateNames

--- client/src/properties/state.rs
+++ client/src/properties/state.rs
@@ -1,14 +1,16 @@
-use crate::properties::data::TPData;
+use crate::{contract::ContractID, properties::data::TPData};
 
 use std::marker::PhantomData;
 use typemap::TypeMap;
 
 pub type StateID<T> = arena::Index<State<T>>;
 
-/// Represents a particular state of a contract. For actual state data of a
-/// specific object, see [`StateID`].
-pub struct StateProperty {
-    idx: usize,
+/// Represents a particular state field of a contract. For actual state data of
+/// a specific object, see [`StateID`].
+#[derive(Copy, Clone, Hash, Debug)]
+pub struct StateName {
+    idx: usize, // idx into an object's state properties
+    contract: ContractID,
 }
 
 pub struct State<T: TPData>(T);
@@ -19,4 +21,4 @@ impl<T: 'static + TPData> typemap::Key for StateArenaID<T> {
     type Value = arena::Arena<State<T>>;
 }
 
-pub type StateArenaMap = TypeMap<arena::Arena<State<dyn TPData>>>;
+pub type StateArenaMap = TypeMap;
