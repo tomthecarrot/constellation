e32e433ed180421eac828489e3b712a26810d401
Ryan Butler <thebutlah@gmail.com>
2021-11-19 16:11:36 -0500

    Added initial hello world. Does not compile to WASM yet

---
+++ circle_game/src/wasm_main.rs
@@ -0,0 +1,14 @@
+#[wasm_bindgen]
+pub fn main() {
+    let mut app = App::build();
+
+    app.add_plugins(DefaultPlugins);
+
+    // when building for Web, use WebGL2 rendering
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    // TODO: add all your other stuff to `app` as usual
+
+    app.run();
+}
