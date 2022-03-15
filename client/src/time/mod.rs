f85fed7295d1726891eeaad7c7ec33a1ee57747d
Thomas Suarez <code@tomthecarrot.com>
2022-03-15 15:30:58 -0700

    [SER-323] Add `resolve_time()`, remove getters/setters.

--- client/src/time/mod.rs
+++ client/src/time/mod.rs
@@ -1,78 +1,48 @@
-trait ITime {}
+use std::{fmt::Error, num::TryFromIntError};
 
-trait IRealmTime: ITime {
-    fn get_time(&self) -> u32;
-    fn set_time(&mut self, new_time: u32);
+use derive_more::From;
+
+#[derive(From, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Time(u32);
+
+impl Time {
+    pub fn ticks(&self) -> u32 {
+        self.0
+    }
+    pub fn to_seconds(&self) -> f32 {
+        (self.0 as f32) / 1024.0
+    }
 }
 
-trait IObjectTime: ITime {
-    fn get_offset_parent(&self) -> u32;
-    fn set_offset_parent(&mut self, new_offset: u32);
+#[derive(From, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RealmTime(pub Time);
 
-    fn get_offset_local(&self) -> u32;
-    fn set_offset_local(&mut self, new_offset: u32);
-
-    fn get_scalar(&self) -> i32;
-    fn set_scalar(&mut self, new_scalar: i32);
-
-    fn get_interval(&self) -> u32;
-    fn set_interval(&mut self, new_interval: u32);
-}
-
-pub struct RealmTime {
-    time: u32,
-}
-
+#[derive(From, Debug, Clone)]
 pub struct ObjectTime {
-    offset_parent: u32,
-    offset_local: u32,
-    scalar: i32,
-    interval: u32,
+    pub offset_parent: Time,
+    pub offset_local: Time,
+    pub scalar: i32,
+    pub interval: u32,
 }
 
-impl ITime for RealmTime {}
-impl ITime for ObjectTime {}
+impl ObjectTime {
+    pub fn resolve_time(&self, parent_time: &Time) -> Time {
+        // See v3 Spec: Time: ObjectTime for documentation on this formula.
+        let mut parent_diff_precise: i64 =
+            (parent_time.ticks() - self.offset_parent.ticks()) as i64;
+        parent_diff_precise *= self.scalar as i64;
+        parent_diff_precise >>= 10;
+        parent_diff_precise += self.offset_local.ticks() as i64;
+        parent_diff_precise %= self.interval as i64;
 
-impl IRealmTime for RealmTime {
-    fn get_time(&self) -> u32 {
-        self.time
-    }
-
-    fn set_time(&mut self, new_time: u32) {
-        self.time = new_time;
-    }
-}
-
-impl IObjectTime for ObjectTime {
-    fn get_offset_parent(&self) -> u32 {
-        self.offset_parent
-    }
-
-    fn set_offset_parent(&mut self, new_offset: u32) {
-        self.offset_parent = new_offset;
-    }
-
-    fn get_offset_local(&self) -> u32 {
-        self.offset_local
-    }
-
-    fn set_offset_local(&mut self, new_offset: u32) {
-        self.offset_local = new_offset;
-    }
-
-    fn get_scalar(&self) -> i32 {
-        self.scalar
-    }
-
-    fn set_scalar(&mut self, new_scalar: i32) {
-        self.scalar = new_scalar;
-    }
-
-    fn get_interval(&self) -> u32 {
-        self.interval
-    }
-
-    fn set_interval(&mut self, new_interval: u32) {
-        self.interval = new_interval;
+        // Convert from i64 -> u32
+        let result: Result<u32, TryFromIntError> = parent_diff_precise.try_into();
+        match result {
+            Ok(value) => Time(value),
+            Err(err) => {
+                log::error!("ObjectTime could not resolve time: {}", err);
+                Time(0)
+            }
+        }
     }
 }
