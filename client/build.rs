3cfd3f90de1f0691b3fbcc16f4a5c6ffffbccea9
Thomas Suarez <code@tomthecarrot.com>
2022-03-29 17:31:49 -0700

    [SER-237] Add wip experiments.

---
+++ client/build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // cxx_build::bridge("src/lib.rs")
+    //     .flag("-std=c++14")
+    //     .compile("teleportal-platform");
+}
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -10,11 +10,16 @@ pub mod object;
 pub mod realm;
 pub mod time;
 
+use contract::properties::states::{StateHandle, StateId};
+use contract::ContractDataHandle;
 use eyre::{eyre, Result};
 use link::{Link, RealmServerHandle, Session};
+use object::ObjectHandle;
 use realm::{Realm, RealmID};
 
 pub use engine::Engine;
+
+use lazy_mut::lazy_mut;
 
 #[non_exhaustive]
 pub enum Endpoint {
@@ -64,3 +69,63 @@ impl RealmClient {
         Ok(result)
     }
 }
+
+// #[cxx::bridge]
+// mod ffi {
+//     extern "Rust" {
+//         type Engine;
+//         fn init() -> Engine;
+//         fn get_u8() -> u8;
+//     }
+// }
+
+// lazy_mut! {
+//     static mut ENGINE: Engine = Engine::new(Realm::new(RealmID::new(String::from("main"))), None).0;
+// }
+
+#[repr(C)]
+pub struct EngineOwnedByC {
+    _engine: Engine,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn teleportal_engine_init() -> Box<Engine> {
+    let engine = Engine::new(Realm::new(RealmID::new(String::from("main"))), None).0;
+    Box::from(engine)
+}
+
+#[no_mangle]
+pub extern "C" fn teleportal_engine_get_u8() -> u8 {
+    10
+}
+
+#[no_mangle]
+pub extern "C" fn teleportal_engine_get_state_handle_u8(
+    engine: Box<Engine>,
+    object_handle: Box<ObjectHandle>,
+    contract_handle: Box<ContractDataHandle>,
+    state_idx: usize,
+) -> Box<StateHandle<u8>> {
+    if let Ok(object) = engine
+        .realm()
+        .baseline(baseline::BaselineKind::Fork)
+        .object(*object_handle)
+    {
+        let state_id = StateId::new(state_idx, *contract_handle);
+        let state_handle = object.state(state_id);
+        Box::from(state_handle)
+    } else {
+        panic!("TODO");
+    }
+}
+
+// #[no_mangle]
+// pub unsafe fn tp_get_state_value_u8(object_handle: ObjectHandle, state_handle: usize) -> u8 {
+//     if let Ok(state) = common_realm_client
+//         .local_realm
+//         .baseline(baseline::BaselineKind::Fork)
+//         .state(state_handle)
+//     {
+//         state.0
+//     }
+// }
