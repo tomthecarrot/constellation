250dbaa8accd3a5d226f28d9dc20a9e57e53d725
Thomas Suarez <code@tomthecarrot.com>
2022-06-21 14:32:17 +0100

    [SER-357] Use package names in build script args.

--- client/codegen_mac_arm64.sh
+++ client/codegen_mac_arm64.sh
@@ -11,8 +11,11 @@ rm -rf $CLIENT_DIR/cs/src/generated
 rm -rf $RSHARP_DIR/cs/src/generated
 
 cd $PLATFORM_DIR
-cargo build
-cargo build --target x86_64-apple-darwin
+cargo build -p tp_client
+cargo build -p tp_client --target x86_64-apple-darwin
+
+cargo build -p rsharp
+cargo build -p rsharp --target x86_64-apple-darwin
 
 if [ ! -f $CLIENT_DIR/cs/tests/bin/Debug/net6.0/libtp_client.dylib ]; then
     ln -s $PLATFORM_DIR/target/debug/deps/libtp_client.dylib $CLIENT_DIR/cs/tests/bin/Debug/net6.0/libtp_client.dylib
@@ -25,11 +28,8 @@ fi
 mv $PLATFORM_DIR/target/x86_64-apple-darwin/debug/deps/libtp_client.dylib $PLATFORM_DIR/target/x86_64-apple-darwin/debug/deps/libtp_client
 mv $PLATFORM_DIR/target/x86_64-apple-darwin/debug/deps/librsharp.dylib $PLATFORM_DIR/target/x86_64-apple-darwin/debug/deps/librsharp
 
-cd $CLIENT_DIR/rust/
-cargo test
-
-cd $RSHARP_DIR/rust
-cargo test
+cargo test -p tp_client
+cargo test -p rsharp
 
 cd $CLIENT_DIR/codegen_pinvoke
 dotnet run -a x64
