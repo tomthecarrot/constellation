9b67de99c3ef9986988e6058c499eac5026bfb7f
Ryan Butler <thebutlah@gmail.com>
2022-07-20 20:09:11 -0400

    [SER-423] Added C# bindings for BallStates (#103)
    Added C# bindings for BallStates

--- demos/unity_states/cs/BallStates.cs
+++ demos/unity_states/cs/BallStates.cs
@@ -1,4 +1,6 @@
 using RSharp;
+using States = Teleportal.Client.Contract.Properties.States;
+using ffi = unity_states.generated.__Internal;
 
 /// NOTE: this class is only ever a shared ref in rust
 public struct BallStates
@@ -10,5 +12,83 @@ public struct BallStates
         this.inner = inner;
     }
 
-    // TODO: implement StateId accessors
+    States.StateId_F32 PosX
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_pos_x(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_F32 PosY
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_pos_y(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_F32 PosZ
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_pos_z(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_I16 EulerX
+    {
+        get => new States.StateId_I16(
+            new Ptr<States.StateId_I16>(ffi.BallStates_euler_x(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_I16 EulerY
+    {
+        get => new States.StateId_I16(
+            new Ptr<States.StateId_I16>(ffi.BallStates_euler_y(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_I16 EulerZ
+    {
+        get => new States.StateId_I16(
+            new Ptr<States.StateId_I16>(ffi.BallStates_euler_z(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_F32 ScaleX
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_scale_x(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_F32 ScaleY
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_scale_y(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_F32 ScaleZ
+    {
+        get => new States.StateId_F32(
+            new Ptr<States.StateId_F32>(ffi.BallStates_scale_z(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
+
+    States.StateId_U64 Color
+    {
+        get => new States.StateId_U64(
+            new Ptr<States.StateId_U64>(ffi.BallStates_color(this.inner.p)),
+            OwnershipSemantics.Owned
+        );
+    }
 }
