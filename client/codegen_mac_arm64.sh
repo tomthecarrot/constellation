48b9af4aa393b46b852da06e0c4653f3ed93c876
Thomas Suarez <code@tomthecarrot.com>
2022-08-07 17:42:32 -0700

    [SER-447] Strip dylib extension for symlink compatibility.

--- client/codegen_mac_arm64.sh
+++ client/codegen_mac_arm64.sh
@@ -10,19 +10,28 @@ RSHARP_DIR="$PLATFORM_DIR/crates/rsharp"
 rm -rf $CLIENT_DIR/cs/src/generated
 rm -rf $RSHARP_DIR/cs/src/generated
 
+# Build Platform core and demo libraries.
 cd $PLATFORM_DIR
 cargo build -p tp_client
 cargo build -p rsharp
 cargo build -p unity_states
 
-mv $PLATFORM_DIR/target/debug/deps/libtp_client.dylib $CLIENT_DIR/cs/tests/bin/Debug/net6.0/libtp_client
-mv $PLATFORM_DIR/target/debug/deps/librsharp.dylib $CLIENT_DIR/cs/tests/bin/Debug/net6.0/librsharp
-mv $PLATFORM_DIR/target/debug/deps/libunity_states.dylib $CLIENT_DIR/cs/tests/bin/Debug/net6.0/libunity_states
+# Strip .dylib extension for symlink compatibility across OS targets.
+mv $PLATFORM_DIR/target/debug/deps/libtp_client.dylib $PLATFORM_DIR/target/debug/deps/libtp_client
+mv $PLATFORM_DIR/target/debug/deps/librsharp.dylib $PLATFORM_DIR/target/debug/deps/librsharp
+mv $PLATFORM_DIR/target/debug/deps/libunity_states.dylib $PLATFORM_DIR/target/debug/deps/libunity_states
 
+# Copy Platform libraries for C# tests.
+cp $PLATFORM_DIR/target/debug/deps/libtp_client $CLIENT_DIR/cs/tests/bin/Debug/net6.0/
+cp $PLATFORM_DIR/target/debug/deps/librsharp $CLIENT_DIR/cs/tests/bin/Debug/net6.0/
+cp $PLATFORM_DIR/target/debug/deps/libunity_states $CLIENT_DIR/cs/tests/bin/Debug/net6.0/
+
+# Generate C bindings.
 cargo test -p tp_client
 cargo run -p rsharp
 cargo test -p unity_states
 
+# Generate C# bindings.
 cd $CLIENT_DIR/codegen_pinvoke
 dotnet run -a x64 # CppSharp is compiled for x64.
 
@@ -32,5 +41,6 @@ cargo run
 cd $CLIENT_DIR/cs/tests
 dotnet test
 
+# Package DLLs for use in Unity demo.
 cd $PLATFORM_DIR/demos/unity_states/cs
 dotnet build
