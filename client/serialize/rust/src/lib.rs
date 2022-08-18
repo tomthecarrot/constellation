ed25e79b540321e59b1f2d08219363622fc39bf0
Ryan Butler <thebutlah@gmail.com>
2022-08-18 16:30:29 -0400

    Finished deserialization except handle types

--- client/serialize/rust/src/lib.rs
+++ client/serialize/rust/src/lib.rs
@@ -1,10 +1,13 @@
+#[allow(unused)]
 mod generated;
+
 mod impls;
 
 pub use generated::tp_serialize::*;
 
 use bimap::BiHashMap;
 use eyre::{eyre, Result, WrapErr};
+use tp_client::contract::properties::dynamic::{DynTpPrimitive, DynTpProperty};
 use tp_client::contract::properties::states::IStates;
 
 /// The types related to the tp_client
@@ -17,6 +20,8 @@ mod c {
 /// The types related to the flatbuffer
 mod t {
     pub use crate::baseline::Baseline;
+    pub use crate::generated::tp_serialize::object::Object;
+    pub use crate::generated::tp_serialize::primitive::TpPrimitive;
 }
 
 pub struct Deserializer<'a> {
@@ -105,8 +110,8 @@ impl<'a> Deserializer<'a> {
         };
 
         // Create all objects associated with the contract.
-        // Uses helper closure just to enable early returns.
-        let helper = || -> Result<()> {
+        // Uses helper closure just to enable early returns and keep variables local.
+        let mut helper = || -> Result<()> {
             let objects_t = if let Some(o) = baseline_t.objects() {
                 o
             } else {
@@ -116,10 +121,11 @@ impl<'a> Deserializer<'a> {
             let contract_idx = *self.contract_idxs.get_by_left(&C::ID).unwrap();
             let num_states = C::States::enumerate_types().len();
 
-            objects_t
+            // Filter to just the objects that match our contract
+            // usize is the index into the flatbuffer's `Baseline.states` vec.
+            let matching_objects: Vec<(usize, t::Object)> = objects_t
                 .into_iter()
                 .enumerate()
-                // Filter to just the objects that match our contract
                 .filter(|(_idx, obj)| {
                     let c = if let Some(c) = obj.contract() {
                         c
@@ -128,45 +134,109 @@ impl<'a> Deserializer<'a> {
                     };
                     c.idx() as usize == contract_idx
                 })
-                // create states and object in baseline
-                .try_for_each(|(idx, obj_t)| -> Result<()> {
-                    // Validate that number of states in object matches contract
-                    let obj_states_t = {
-                        obj_t.states();
-                        if obj_t.states().map_or(0, |x| x.len()) != num_states {
-                            return Err(eyre!(
-                                "number of states in serialized object did not match contract"
-                            ));
-                        }
-                        if let Some(s) = obj_t.states() {
-                            s
-                        } else {
-                            return Ok(());
-                        }
-                    };
+                .collect();
 
-                    // Go through the object's states and validate that they match the
-                    // contract
-                    for (i, (obj_state_t, typ)) in
-                        std::iter::zip(obj_states_t, C::States::enumerate_types().into_iter())
-                            .enumerate()
-                    {
-                        let states_t = baseline_t
-                            .states()
-                            .ok_or(eyre!("Expected at least one state!"))?;
-                        let state_t = states_t.get(obj_state_t.idx() as usize);
-                        if state_t.p_type() != *typ {
-                            return Err(eyre!(
-                                "state #{i} type was {:?} but expected {:?}",
-                                state_t.p_type().variant_name().unwrap(),
-                                *typ,
-                            ));
-                        }
+            // Validate all the states for each object
+            for (_idx, obj_t) in matching_objects.iter() {
+                // Check that # of states match contract
+                let obj_states_t = {
+                    obj_t.states();
+                    if obj_t.states().map_or(0, |x| x.len()) != num_states {
+                        return Err(eyre!(
+                            "number of states in serialized object did not match contract"
+                        ));
+                    }
+                    if let Some(s) = obj_t.states() {
+                        s
+                    } else {
+                        // no states in this object, skip to the next one
+                        continue;
+                    }
+                };
+
+                // Check that state types match contract
+                for (i, (obj_state_t, typ)) in
+                    std::iter::zip(obj_states_t, C::States::enumerate_types().into_iter())
+                        .enumerate()
+                {
+                    let states_t = baseline_t
+                        .states()
+                        .ok_or(eyre!("Expected at least one state!"))?;
+                    let state_t = states_t.get(obj_state_t.idx() as usize);
+                    if state_t.p_type() != *typ {
+                        return Err(eyre!(
+                            "state #{i} type was {:?} but expected {:?}",
+                            state_t.p_type().variant_name().unwrap(),
+                            *typ,
+                        ));
+                    }
+                }
+            }
+
+            for (_idx, obj_t) in matching_objects {
+                let obj_states_t = if let Some(s) = obj_t.states() {
+                    s
+                } else {
+                    continue;
+                };
+
+                // Build the vec of props that we will use to create the object
+                let mut dyn_props: Vec<DynTpProperty> = Vec::with_capacity(obj_states_t.len());
+                for obj_state_t in obj_states_t {
+                    // convert StateHandle to State
+                    let obj_state_t = baseline_t.states().unwrap().get(obj_state_t.idx() as usize);
+
+                    macro_rules! helper {
+                        ($e:expr) => {{
+                            let prop =
+                                DynTpProperty::Primitive(DynTpPrimitive::from($e.to_owned()));
+                            dyn_props.push(prop);
+                        }};
                     }
 
-                    // TODO: Actually create the states
-                    Ok(())
-                })?;
+                    // Handle dynamic typing of union to access the property
+                    use t::TpPrimitive as P;
+                    match obj_state_t.p_type() {
+                        P::U8 => helper!(obj_state_t.p_as_u8().unwrap().v()),
+                        P::U16 => helper!(obj_state_t.p_as_u16().unwrap().v()),
+                        P::U32 => helper!(obj_state_t.p_as_u32().unwrap().v()),
+                        P::U64 => helper!(obj_state_t.p_as_u64().unwrap().v()),
+                        P::I8 => helper!(obj_state_t.p_as_i8().unwrap().v()),
+                        P::I16 => helper!(obj_state_t.p_as_i16().unwrap().v()),
+                        P::I32 => helper!(obj_state_t.p_as_i32().unwrap().v()),
+                        P::I64 => helper!(obj_state_t.p_as_i64().unwrap().v()),
+                        P::Bool => helper!(obj_state_t.p_as_bool().unwrap().v()),
+                        P::F32 => helper!(obj_state_t.p_as_f32().unwrap().v()),
+                        P::F64 => helper!(obj_state_t.p_as_f64().unwrap().v()),
+                        P::String => helper!(obj_state_t.p_as_string().unwrap().v().unwrap()),
+                        P::tp_serialize_object_ObjectHandle => {
+                            obj_state_t
+                                .p_as_tp_serialize_object_object_handle()
+                                .unwrap();
+                            let prop =
+                                DynTpProperty::Primitive(DynTpPrimitive::ObjectHandle(todo!(
+                                    "figure out how to get the actual ObjectHandle from fb index"
+                                )));
+                            dyn_props.push(prop);
+                        }
+                        P::tp_serialize_contract_ContractDataHandle => {
+                            obj_state_t
+                                .p_as_tp_serialize_contract_contract_data_handle()
+                                .unwrap();
+                            let prop =
+                                DynTpProperty::Primitive(DynTpPrimitive::ContractDataHandle(todo!(
+                                    "figure out how to get the actual ContractDataHandle from fb index"
+                                )));
+                            dyn_props.push(prop);
+                        }
+                        _ => unreachable!(),
+                    };
+                }
+
+                self.baseline
+                    .object_create(&contract, dyn_props.into_iter(), [].into_iter())
+                    .wrap_err("failed to create object")?;
+            }
 
             Ok(())
         };
