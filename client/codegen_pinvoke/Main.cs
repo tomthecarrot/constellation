7a1e0a5baabfd6391b828142c37ec7ed369de1ae
Ryan Butler <thebutlah@gmail.com>
2022-09-07 11:30:12 -0700

    [SER-467] Fix codegen_pinvoke deletion behavior being platform specific (#129)
    Fix platform-specific codegen_pinvoke behavior

--- client/codegen_pinvoke/Main.cs
+++ client/codegen_pinvoke/Main.cs
@@ -8,6 +8,9 @@ using DirectoryInfo = System.IO.DirectoryInfo;
 using Path = System.IO.Path;
 using Dir = System.IO.Directory;
 using System.Collections.Generic;
+using RtInfo = System.Runtime.InteropServices.RuntimeInformation;
+using OS = System.Runtime.InteropServices.OSPlatform;
+using Exception = System.Exception;
 
 
 namespace Codegen
@@ -29,20 +32,27 @@ namespace Codegen
         }
     }
 
-    public class Codegen : CppSharp.ILibrary
+    public sealed class Codegen : CppSharp.ILibrary
     {
         private static DirectoryInfo project_dir = GetProjectDir();
-        private LibInfo lib_info;
-        private const bool overwrite = true;
+        private readonly LibInfo lib_info;
+        private readonly static string DYLIB_EXTENSION =
+            (RtInfo.IsOSPlatform(OS.Linux) || RtInfo.IsOSPlatform(OS.FreeBSD)) ? ".so"
+            : RtInfo.IsOSPlatform(OS.Windows) ? ".dll"
+            : RtInfo.IsOSPlatform(OS.OSX) ? ".dylib"
+            : throw new Exception("unknown platform");
 
         static int Main(string[] args)
         {
+            const bool OVERWRITE = true;
+
             if (args.Length > 0)
             {
                 Console.Error.WriteLine("Too many arguments!");
                 return -1;
             }
 
+            // Configure information about the libs we will be generating
             var libs = new List<LibInfo>();
             libs.Add(new LibInfo(
                 output_dir: Path.Join(
@@ -66,22 +76,31 @@ namespace Codegen
 
             foreach (var lib in libs)
             {
+                Console.WriteLine($"Generating c# pinvoke for {lib.crate_name}");
 
-                if (!Codegen.overwrite && (lib.output_dir.GetDirectories().Length != 0 || lib.output_dir.GetFiles().Length != 0))
+                // Handle potentially overwriting existing `output_dir`
+                var has_files = lib.output_dir.Exists && (
+                    lib.output_dir.GetDirectories().Length != 0
+                    || lib.output_dir.GetFiles().Length != 0
+                );
+                if (!OVERWRITE && has_files)
                 {
                     Console.Error.WriteLine("Output directory must be empty.");
                     return -1;
                 }
+                else if (OVERWRITE && has_files)
+                {
+                    lib.output_dir.Delete(true);
+                }
 
-                lib.output_dir.Delete(true); // macOS: comment this line.
-
-                CppSharp.ConsoleDriver.Run(new Codegen(lib));
+                // Actually generate the code
+                CppSharp.ConsoleDriver.Run(new Codegen(lib_info: lib));
             }
 
             return 0;
         }
 
-        public Codegen(LibInfo lib_info)
+        private Codegen(LibInfo lib_info)
         {
             this.lib_info = lib_info;
         }
@@ -99,8 +118,7 @@ namespace Codegen
             module.IncludeDirs.Add(this.lib_info.input_dir.FullName);
             module.Headers.Add("generated.h");
             module.LibraryDirs.Add(this.lib_info.cargo_artifact_dir.FullName);
-            module.Libraries.Add($"lib{this.lib_info.crate_name}.so"); // macOS: remove .so extension.
-            // module.Undefines.Add("__cplusplus");
+            module.Libraries.Add($"lib{this.lib_info.crate_name}{Codegen.DYLIB_EXTENSION}");
         }
 
         /// Setup your passes here.
@@ -115,16 +133,29 @@ namespace Codegen
         /// Get the toplevel folder in the project
         public static DirectoryInfo GetProjectDir()
         {
+            // If we are compiling for a target different from the native platform,
+            // this will be nested one folder deeper (to disambiguate the platform).
+            // So this code will check two folders and pick one based on the expected
+            // folder layout.
+
             var assembly_file = new DirectoryInfo(Rflct.Assembly.GetExecutingAssembly().Location);
 
             DirectoryInfo project_dir = assembly_file;
-            const uint n_steps_up = 6; // macOS: 7 steps up.
+            const uint n_steps_up = 5; // macOS: 7 steps up.
             for (var i = 0; i < n_steps_up; i++)
             {
                 project_dir = project_dir.Parent ?? project_dir.Root;
             }
 
-            return project_dir;
+            // We are now either at the `client` folder or one folder deeper if
+            // compiling to a non-native architecture. Lets figure out which one.
+            if (project_dir.Name != "client")
+            {
+                // Yes, this is a dirty hack.
+                project_dir = project_dir.Parent ?? project_dir.Root;
+            }
+            // Go up one more to reach the toplevel directory that holds all the projects
+            return project_dir.Parent ?? project_dir.Root;
         }
     }
 
