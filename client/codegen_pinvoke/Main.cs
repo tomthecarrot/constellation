5f35e4e970d0b755bd8302cff4643d471c5f35f8
Thomas Suarez <code@tomthecarrot.com>
2022-09-12 16:40:01 -0700

    [SER-473] Support Movieoke codegen_pinvoke
    wip
    
    temp
    
    Implement Display for rsharp::string::String.
    
    Remove Display trait from string.
    
    Append && to each build script line.
    
    Append `native` to movieoke lib name.
    
    Rename all libraries to `constellation` for symbol compatibility.
    
    Export lib w/o dylib extension
    
    Remove && from line endings.
    
    Address PR comments.
    
    Move accessor/binding and string update to #136.
    
    Bring back .dylib extension in build script.

--- client/codegen_pinvoke/Main.cs
+++ client/codegen_pinvoke/Main.cs
@@ -34,6 +34,13 @@ namespace Codegen
     {
         private static DirectoryInfo project_dir = GetProjectDir();
         private readonly LibInfo lib_info;
+        private string single_lib_name;
+        private readonly static string DYLIB_EXTENSION =
+            (RtInfo.IsOSPlatform(OS.Linux) || RtInfo.IsOSPlatform(OS.FreeBSD)) ? ".so"
+            : RtInfo.IsOSPlatform(OS.Windows) ? ".dll"
+            // Using ".dylib" on mac silently inhibits DLLImports 🤦‍♂️
+            : RtInfo.IsOSPlatform(OS.OSX) ? ""
+            : throw new Exception("unknown platform");
 
         static int Main(string[] args)
         {
@@ -85,16 +92,20 @@ namespace Codegen
                     lib.output_dir.Delete(true);
                 }
 
+                // TODO: move this string somewhere more sensible
+                const string single_lib_name = "constellation";
+
                 // Actually generate the code
-                CppSharp.ConsoleDriver.Run(new Codegen(lib_info: lib));
+                CppSharp.ConsoleDriver.Run(new Codegen(lib, single_lib_name));
             }
 
             return 0;
         }
 
-        private Codegen(LibInfo lib_info)
+        public Codegen(LibInfo lib_info, string single_lib_name)
         {
             this.lib_info = lib_info;
+            this.single_lib_name = single_lib_name;
         }
 
         /// Setup the driver options here.
@@ -106,10 +117,8 @@ namespace Codegen
             options.GenerateClassTemplates = false;
             options.GenerateFinalizers = false;
 
-            // hard coding "unity_states" as a stopgap until we decide which target
-            // provides all the symbols for the necessary libraries
-            var module = options.AddModule("unity_states");
-            module.Libraries.Add($"libunity_states");
+            var module = options.AddModule(this.single_lib_name);
+            module.Libraries.Add($"lib{this.single_lib_name}");
             module.OutputNamespace = this.lib_info.crate_name;
 
             module.IncludeDirs.Add(this.lib_info.input_dir.FullName);
@@ -146,7 +155,10 @@ namespace Codegen
             // We are now either at the `client` folder or one folder deeper if
             // compiling to a non-native architecture. Lets figure out which one.
             // Yes, this is a dirty hack.
-            if (project_dir.Name != "client")
+            // The "client" directory comes from this repo (Constellation).
+            // The "codegen_movieoke" directory is added temporarily to support codegen
+            // in the Movieoke repo, which references this codegen csproj.
+            if (project_dir.Name != "client" && project_dir.Name != "codegen_movieoke")
             {
                 project_dir = project_dir.Parent ?? project_dir.Root;
             }
