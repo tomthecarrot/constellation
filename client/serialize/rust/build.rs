53d9a02a4656fb5cd7da9d452bd47ae293b396a9
Ryan Butler <thebutlah@gmail.com>
2022-09-19 14:47:22 -0400

    [SER-474] Flatbuffer codegen uses cargo build script (#135)
    * Deleted obsolete serialization folder
    
    * Now using dynpath

---
+++ client/serialize/rust/build.rs
@@ -0,0 +1,14 @@
+use std::path::{Path, PathBuf};
+
+fn main() -> flatc_rust::Result<()> {
+    println!("cargo:rerun-if-changed=../flatbuffers");
+
+    flatc_rust::run(flatc_rust::Args {
+        inputs: &[&Path::new("../flatbuffers/all.fbs")],
+        out_dir: PathBuf::from(format!("{}", std::env::var("OUT_DIR").unwrap())).as_path(),
+        extra: &["--gen-all", "--rust-module-root-file"],
+        ..Default::default()
+    })?;
+
+    Ok(())
+}
