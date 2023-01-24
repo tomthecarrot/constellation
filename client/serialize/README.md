fd65c957974740a6d62f96ccd3a13e0b7e004974
Ryan Butler <thebutlah@gmail.com>
2023-01-23 20:18:04 -0500

    Check flatc version

--- client/serialize/README.md
+++ client/serialize/README.md
@@ -2,6 +2,6 @@
 
 ## Generating flatbuffers
 
-1. Get flatc
+1. Get flatc v23.1.21 (must be exact version) on your path
 1. cd to `serialize/rust/src/generated` dir
 1. run `flatc --rust ../../../flatbuffers/all.fbs --gen-all --rust-module-root-file`
--- client/serialize/rust/build.rs
+++ client/serialize/rust/build.rs
@@ -2,8 +2,14 @@ use std::path::{Path, PathBuf};
 
 fn main() -> flatc_rust::Result<()> {
     println!("cargo:rerun-if-changed=../flatbuffers");
+    let flatc = flatc_rust::Flatc::from_env_path();
+    assert_eq!(
+        flatc.version().unwrap().version(),
+        "23.1.21",
+        "flatc was wrong version."
+    );
 
-    flatc_rust::run(flatc_rust::Args {
+    flatc.run(flatc_rust::Args {
         inputs: &[&Path::new("../flatbuffers/all.fbs")],
         out_dir: PathBuf::from(format!("{}", std::env::var("OUT_DIR").unwrap())).as_path(),
         extra: &["--gen-all", "--rust-module-root-file"],
