27006fed56d827aa05fe0decc40287d1e17d405a
Ryan Butler <thebutlah@gmail.com>
2023-02-01 16:58:07 -0500

    Added assertions for handles too

--- client/serialize/rust/tests/round_trip.rs
+++ client/serialize/rust/tests/round_trip.rs
@@ -1,10 +1,11 @@
-use tp_serialize::{Deserializer, DeserializerBuilder, Serializer};
+use tp_client::object::ObjectHandle;
+use tp_serialize::{DeserializerBuilder, Serializer};
 
 use eyre::WrapErr;
 use flatbuffers::FlatBufferBuilder;
 use tp_client::baseline::{Baseline, BaselineKind};
 use tp_client::contract::properties::dynamic::DynTpProperty;
-use tp_client::contract::{Contract, ContractDataHandle, ContractId};
+use tp_client::contract::{Contract, ContractData, ContractDataHandle, ContractId};
 use tp_contract_example::ExampleContract;
 
 struct EmptyContract {
@@ -50,6 +51,8 @@ struct Fields {
 
 #[test]
 fn test_round_trip() -> eyre::Result<()> {
+    let _ = color_eyre::install();
+
     let mut fields = Vec::new();
     for i in 0..10 {
         let f = Fields {
@@ -66,8 +69,8 @@ fn test_round_trip() -> eyre::Result<()> {
     }
 
     let (empty_contract, example_contract, baseline) = create_baseline(&fields);
-    check_matches_fields(&fields, &example_contract, &baseline)
-        .wrap_err("`create_baseline` doesn't seem to be correct.")?;
+    check_matches_fields(&fields, &empty_contract, &example_contract, &baseline)
+        .wrap_err("Serialized baseline and contracts didn't match expected fields")?;
 
     let bytes = {
         let mut serializer = Serializer::new(FlatBufferBuilder::new(), &baseline);
@@ -103,26 +106,15 @@ fn test_round_trip() -> eyre::Result<()> {
         (de_empty_contract, de_example_contract, de_baseline)
     };
 
-    // Validate that EmptyContract and its objects were deserialized propery
-    {
-        let cd = de_baseline.contract_data(de_empty_contract.handle())?;
-        assert_eq!(
-            cd.id(),
-            EmptyContract::ID,
-            "ID of deserialized EmptyContract did not match"
-        );
-        assert_eq!(
-            cd.objects().len(),
-            1,
-            "Expected deserialized EmptyContract to have exactly 1 object"
-        );
-    }
-
     // Validate that all ExampleContract objects in the two baselines match
     // TODO: Currently this does not check if handle types are correct!
-    check_matches_fields(&fields, &de_example_contract, &de_baseline)?;
-    check_matches_fields(&fields, &example_contract, &de_baseline)?;
-    check_matches_fields(&fields, &de_example_contract, &baseline)?;
+    check_matches_fields(
+        &fields,
+        &de_empty_contract,
+        &de_example_contract,
+        &de_baseline,
+    )
+    .wrap_err("Deserialized baseline and contracts didn't match expected fields")?;
 
     Ok(())
 }
@@ -159,37 +151,78 @@ fn create_baseline(fields: &[Fields]) -> (EmptyContract, ExampleContract, Baseli
     (empty_c, c, b)
 }
 
-fn check_matches_fields(fields: &[Fields], c: &ExampleContract, b: &Baseline) -> eyre::Result<()> {
+fn check_matches_fields(
+    fields: &[Fields],
+    c_empty: &EmptyContract,
+    c_example: &ExampleContract,
+    b: &Baseline,
+) -> eyre::Result<()> {
     let mut fields = fields.to_vec();
     let cd = b
-        .contract_data(c.handle())
-        .expect("Failed to get contract data from handle");
+        .contract_data(c_example.handle())
+        .wrap_err("failed to get ContractData for ExampleContract")?;
 
-    assert_eq!(fields.len(), cd.objects().len(), "# of objects mismatched");
+    // Validate that ExampleContract deserialized correctly
+    {
+        assert_eq!(
+            cd.id(),
+            ExampleContract::ID,
+            "ID of ExampleContract did not match"
+        );
+
+        assert_eq!(
+            cd.objects().len(),
+            fields.len(),
+            "# of ExampleContract objects mismatched"
+        );
+    }
+
+    // Validate that EmptyContract and its objects were deserialized propery
+    {
+        let cd = b
+            .contract_data(c_empty.handle())
+            .wrap_err("Failed to get ContractData for EmptyContract")?;
+        assert_eq!(
+            cd.id(),
+            EmptyContract::ID,
+            "ID of deserialized EmptyContract did not match"
+        );
+        assert_eq!(
+            cd.objects().len(),
+            1,
+            "Expected deserialized EmptyContract to have exactly 1 object"
+        );
+    }
 
     for obj_h in cd.objects() {
         let obj_h = *obj_h;
         let u8_0_h = b
-            .bind_state(c.states().u8_0(), obj_h)
+            .bind_state(c_example.states().u8_0(), obj_h)
             .expect("Failed to bind u8_0");
         let u8_1_h = b
-            .bind_state(c.states().u8_1(), obj_h)
+            .bind_state(c_example.states().u8_1(), obj_h)
             .expect("Failed to bind u8_1");
         let i8_0_h = b
-            .bind_state(c.states().i8_0(), obj_h)
+            .bind_state(c_example.states().i8_0(), obj_h)
             .expect("Failed to bind i8_0");
         let i8_1_h = b
-            .bind_state(c.states().i8_1(), obj_h)
+            .bind_state(c_example.states().i8_1(), obj_h)
             .expect("Failed to bind i8_1");
         let f32_0_h = b
-            .bind_state(c.states().f32_0(), obj_h)
+            .bind_state(c_example.states().f32_0(), obj_h)
             .expect("Failed to bind f32_0");
         let f32_1_h = b
-            .bind_state(c.states().f32_1(), obj_h)
+            .bind_state(c_example.states().f32_1(), obj_h)
             .expect("Failed to bind f32_1");
         let str_0_h = b
-            .bind_state(c.states().str_0(), obj_h)
+            .bind_state(c_example.states().str_0(), obj_h)
             .expect("Failed to bind str_0");
+        let oh_0_h = b
+            .bind_state(c_example.states().oh_0(), obj_h)
+            .expect("Failed to bind oh_0");
+        let ch_0_h = b
+            .bind_state(c_example.states().ch_0(), obj_h)
+            .expect("Failed to bind ch_0");
 
         let baseline_fields = Fields {
             u8_0: b.state(u8_0_h).expect("Failed to get u8_0").value,
@@ -207,6 +240,34 @@ fn check_matches_fields(fields: &[Fields], c: &ExampleContract, b: &Baseline) ->
                 .position(|f| *f == baseline_fields)
                 .expect("baseline contained an unmatched `Fields`"),
         );
+
+        // Check that ch_0 is correct
+        {
+            let ch_0: ContractDataHandle = b.state(ch_0_h).wrap_err("Failed to get ch_0")?.value;
+            let cd: &ContractData = b.contract_data(ch_0)?;
+            assert_eq!(
+                cd.id(),
+                EmptyContract::ID,
+                "The contract that was referenced was not the EmptyContract"
+            );
+            assert_eq!(
+                cd.objects().len(),
+                1,
+                "There should only be one instance of the EmptyContract"
+            );
+        }
+
+        // Check that oh_0 is correct
+        {
+            let oh_0: ObjectHandle = b.state(oh_0_h).wrap_err("Failed to get oh_0")?.value;
+            let obj = b.object(oh_0)?;
+
+            assert_eq!(
+                obj.contract(),
+                c_empty.handle(),
+                "The object that was referenced was not an instance of EmptyContract"
+            );
+        }
     }
 
     // No fields should be left, because all fields were matched with baseline data
